3195a209a6c5aca02a46bea05797aa827e9bf817
suedbn <suedadaban@gmail.com>
2025-02-27 13:04:21 +0300

    Me.java is created

---
+++ src/Dt.java
@@ -0,0 +1,13 @@
+public class Dt {
+    public static void main(String[] args) {
+        int grade = 1;
+        double grade_double = 1.5f;
+        char first = 'A';
+        boolean isok = true;
+
+        System.out.println(grade);
+        System.out.println(grade_double);
+        System.out.println(first);
+        System.out.println(isok);
+    }
+}
